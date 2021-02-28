32d9f6b2243238327ebd6f7582c1f3198572d221
Maxim Lyasnikov <vfrc29@gmail.com>
2021-02-28 16:46:51 +0300

    add android app and apdu parser

---
+++ lib/apdu-parser/README.md
@@ -0,0 +1,77 @@
+# apdu-parser
+
+https://github.com/eIDuy/apdu-parser.git
+
+APDU-parser lets you parse your APDU commands' and APDU responses' hex bytes into their descriptions.
+Project includes default APDU commands and APDU responses default lists with descriptions.
+
+## Usage
+
+```sh
+python apdu_parser.py -i <input_file>
+
+Options:
+  -h, --help  show this help message and exit
+  
+  -i INPUT_FILE, --input=INPUT_FILE specify input file
+  
+  -o OUTPUT_FILE, --output=OUTPUT_FILE  specify output file
+  
+  -c, --commands  specify if input file contains only APDU commands
+  
+  -r, --responses specify if input file contains only APDU responses
+  
+  -C COMMAND_DESCRIPTIONS, --command-descriptions=COMMAND_DESCRIPTIONS  specify custom command descriptions file
+  
+  -R RESPONSE_DESCRIPTIONS, --response-descriptions=RESPONSE_DESCRIPTIONS specify custom response descriptions file
+  
+  -T, --colors	show terminal output in different colors
+```
+  
+# Examples
+
+Basic usage:
+
+```sh
+python apdu_parser.py -i sample_files/default_log.txt
+```
+
+Set output file:
+
+```sh
+python apdu_parser.py -i sample_files/default_log.txt -o outputs/default_log_output.txt
+```
+
+Parse every line in input file as an APDU command:
+
+```sh
+python apdu_parser.py -i sample_files/commands_log.txt -c
+```
+
+Parse every line in input file as an APDU response:
+
+```sh
+python apdu_parser.py -i sample_files/responses_log.txt -r
+```
+
+Set custom command descriptions file:
+
+```sh
+python apdu_parser.py -i sample_files/default_log.txt -C sample_files/custom_command_descriptions.txt
+```
+
+Set custom command responses file:
+
+```sh
+python apdu_parser.py -i sample_files/default_log.txt -R sample_files/custom_responses_descriptions.txt
+```
+
+Show output results in different colors in terminal (works only on Linux / MacOS):
+
+```sh
+python apdu_parser.py -i sample_files/default_log.txt -T
+```
+
+## Requirements
+
+To properly run the apdu-parser, [Python](http://www.python.org/download/) **2.6.x** or **2.7.x** is required.
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 
 #include <nfc/nfc.h>
 #include <nfc/nfc-types.h>
@@ -11,10 +12,10 @@ static nfc_device *pnd = NULL;
 static nfc_context *context;
 
 static void stop_polling(int sig);
+int cardTransmit(nfc_device *pnd, uint8_t *capdu, size_t capdulen, uint8_t *rapdu, size_t *rapdulen);
 
-#define EXIT_FAILURE  1
-#define EXIT_SUCCESS  0
-
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
 
 int main()
 {
@@ -30,10 +31,14 @@ int main()
         {.nmt = NMT_JEWEL, .nbr = NBR_106},
         // {.nmt = NMT_ISO14443BICLASS, .nbr = NBR_106},
     };
-    const size_t szModulations = 6;
+    const size_t szModulations = 5;
 
     nfc_target nt;
     int res = 0;
+
+    const char *acLibnfcVersion = nfc_version();
+
+    printf("libnfc %s\n", acLibnfcVersion);
 
     nfc_init(&context);
     if (context == NULL)
@@ -59,6 +64,8 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+    nfc_device_set_property_bool(pnd, NP_AUTO_ISO14443_4, true);
+
     printf("NFC reader: %s opened\n", nfc_device_get_name(pnd));
     printf("NFC device will poll during %ld ms (%u pollings of %lu ms for %ld modulations)\n", (unsigned long)uiPollNr * szModulations * uiPeriod * 150, uiPollNr, (unsigned long)uiPeriod * 150, szModulations);
     if ((res = nfc_initiator_poll_target(pnd, nmModulations, szModulations, uiPollNr, uiPeriod, &nt)) < 0)
@@ -71,9 +78,46 @@ int main()
 
     if (res > 0)
     {
-        char * buff[10][10000];
+        printf("Target detected!\n");
+        char *buff[10][10000];
         str_nfc_target(&buff, &nt, true);
-        printf(buff[0]);
+        printf("%s\n", buff[0][0]);
+        fflush(stdout);
+
+        uint8_t capdu[264];
+        size_t capdulen;
+        uint8_t rapdu[264];
+        size_t rapdulen;
+        // Select application
+        char *apdu_cmd = "\x00\xA4\x04\x00\x07";
+        // char *apdu_data = "\xd2\x76\x00\x00\x85\x01\x00";
+        char *apdu_data = "\xF0\x01\x02\x03\x04\x05\x06";
+        memcpy(capdu, apdu_cmd, 5);
+        memcpy(capdu + 5, apdu_data, 7);
+        capdulen = 12;
+        rapdulen = sizeof(rapdu);
+        if (cardTransmit(pnd, capdu, capdulen, rapdu, &rapdulen) < 0)
+            exit(EXIT_FAILURE);
+        // if (rapdulen < 2 || rapdu[rapdulen - 2] != 0x90 || rapdu[rapdulen - 1] != 0x00)
+        //     exit(EXIT_FAILURE);
+        printf("Application selected!\n");
+
+        memcpy(capdu, "\x00\x00\x00\x00\x00\x00\x00", 7);
+        capdulen = 7;
+
+        for (int i = 0; i < 10; i++) {
+            printf("Read byte #%i\n", i);
+            rapdulen = sizeof(rapdu);
+            if (cardTransmit(pnd, capdu, capdulen, rapdu, &rapdulen) < 0) {
+                printf("Read error");
+                exit(EXIT_FAILURE);
+            }
+            rapdu[rapdulen] = 0;
+            printf("%s\n", rapdu);
+        }
+
+
+
         printf("Waiting for card removing...");
         fflush(stdout);
         while (0 == nfc_initiator_target_is_present(pnd, NULL))
@@ -92,13 +136,41 @@ int main()
     exit(EXIT_SUCCESS);
 }
 
+int cardTransmit(nfc_device *pnd, uint8_t *capdu, size_t capdulen, uint8_t *rapdu, size_t *rapdulen)
+{
+    int res;
+    size_t szPos;
+    printf("=> ");
+    for (szPos = 0; szPos < capdulen; szPos++)
+    {
+        printf("%02x ", capdu[szPos]);
+    }
+    printf("\n");
+    if ((res = nfc_initiator_transceive_bytes(pnd, capdu, capdulen, rapdu, *rapdulen, 500)) < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *rapdulen = (size_t)res;
+        printf("<= ");
+        for (szPos = 0; szPos < *rapdulen; szPos++)
+        {
+            printf("%02x ", rapdu[szPos]);
+        }
+        printf("\n");
+        return 0;
+    }
+}
+
 static void stop_polling(int sig)
 {
-  (void) sig;
-  if (pnd != NULL)
-    nfc_abort_command(pnd);
-  else {
-    nfc_exit(context);
-    exit(EXIT_FAILURE);
-  }
+    (void)sig;
+    if (pnd != NULL)
+        nfc_abort_command(pnd);
+    else
+    {
+        nfc_exit(context);
+        exit(EXIT_FAILURE);
+    }
 }
